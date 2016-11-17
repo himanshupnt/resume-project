b814f6126e813c1bb98befa25df767b21815b8cd
Himanshu Pant <himzzy4u@gmail.com>
2016-11-16 18:25:44 -0800

    fixed js-lint issues

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,114 +1,103 @@
 var bio = {
-		"name": "Himanshu Pant",
-		"role": "Front-End Developer",
-		"contacts": {
-			"mobile": "123-12345-123",
-			"email": "abc@gmail.com",
-			"github": "himanshupnt",
-			"location": "San Diego, California"
-		},
-		"welcomeMessage": "Hi there! this is just a placeholder text.",
-		"skills": ["HTML5", "CSS", "Jquery", "Javascript"],
-		"image": "images/me.jpg"
+    "name": "Himanshu Pant",
+    "role": "Front-End Developer",
+    "contacts": {
+        "mobile": "123-12345-123",
+        "email": "abc@gmail.com",
+        "github": "himanshupnt",
+        "location": "San Diego, California"
+    },
+    "welcomeMessage": "Hi there! this is just a placeholder text.",
+    "skills": ["HTML5", "CSS", "Jquery", "Javascript"],
+    "image": "images/me.jpg"
 };
 
 var work = {
-    "jobs": [
-      {
+    "jobs": [{
         "employer": "Uartsy",
         "title": "Web Projects Manager",
         "dates": "2012 - 2016",
         "location": "Los Angeles, California",
         "description": "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean."
-    },
-    {
+    }, {
         "employer": "Self",
         "title": "Front-End developer",
         "dates": "2016 onwards",
         "location": "San Diego, California",
         "description": "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean."
-    }
-  ]
+    }]
 };
 
 var projects = {
-    "project": [
-      {
+    "project": [{
         "title": "Project 1",
         "dates": "2013",
         "description": "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.",
         "images": ["images/1.jpg", "images/2.png"]
-    },
-    {
+    }, {
         "title": "Project 2",
         "dates": "2014",
         "description": "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.",
         "images": ["images/2.png", "images/1.jpg"]
-    }
-  ]
+    }]
 };
 
 var education = {
-    "schools": [
-      {
+    "schools": [{
         "name": "School 1",
         "location": "New Delhi, India",
         "degree": "Bachelors",
         "majors": "Science",
         "dates": "2001 - 2005"
-    },
-    {
+    }, {
         "name": "School 2",
         "location": "Mumbai, India",
         "degree": "Masters",
         "majors": "Science",
         "dates": "2005 - 2006"
-    }
-  ],
-    "online courses": [
-      {
+    }],
+    "online courses": [{
         "title": "Front-End Web Developer Nanodegree",
         "school": "udacity",
         "dates": "2016",
         "url": "https://www.udacity.com/course/nd001"
-    }
-  ]
+    }]
 };
 
 
 bio.display = function() {
     var formattedName =
-      HTMLheaderName.replace("%data%", bio.name);
+        HTMLheaderName.replace("%data%", bio.name);
     var formattedRole =
-      HTMLheaderRole.replace("%data%", bio.role);
+        HTMLheaderRole.replace("%data%", bio.role);
     var formattedMobile =
-  		HTMLmobile.replace("%data%", bio.contacts.mobile);
-  	var formattedEmail =
-  		HTMLemail.replace("%data%", bio.contacts.email);
-  	var formattedGithub =
-  		HTMLgithub.replace("%data%", bio.contacts.github);
-  	var formattedLocation =
-  		HTMLlocation.replace("%data%", bio.contacts.location);
+        HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedEmail =
+        HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGithub =
+        HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLocation =
+        HTMLlocation.replace("%data%", bio.contacts.location);
     var formattedImage =
-      HTMLbioPic.replace("%data%", bio.image);
+        HTMLbioPic.replace("%data%", bio.image);
     var formattedMessage =
-      HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
-      $("#header").prepend(formattedRole);
-    	$("#header").prepend(formattedName);
-    	$("#topContacts").prepend(formattedMobile);
-    	$("#topContacts").prepend(formattedEmail);
-    	$("#topContacts").prepend(formattedGithub);
-    	$("#topContacts").prepend(formattedLocation);
-      $("#header").append(formattedImage);
-      $("#header").append(formattedMessage);
-      $("#header").append(HTMLskillsStart);
-      $("#footerContacts").append(formattedMobile, formattedEmail);
+    $("#header").prepend(formattedRole);
+    $("#header").prepend(formattedName);
+    $("#topContacts").prepend(formattedMobile);
+    $("#topContacts").prepend(formattedEmail);
+    $("#topContacts").prepend(formattedGithub);
+    $("#topContacts").prepend(formattedLocation);
+    $("#header").append(formattedImage);
+    $("#header").append(formattedMessage);
+    $("#header").append(HTMLskillsStart);
+    $("#footerContacts").append(formattedMobile, formattedEmail);
 
     for (var skill = 0; skill < bio.skills.length; skill++) {
-      var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
-      $("#skills").append(formattedSkills);
-    };
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+        $("#skills").append(formattedSkills);
+    }
 
 };
 
@@ -136,13 +125,13 @@ projects.display = function() {
         for (var image = 0; image < projects.project[project].images.length; image++) {
             var formattedImage = HTMLprojectImage.replace("%data%", projects.project[project].images[image]);
             $(".project-entry:last").append(formattedImage);
-        };
+        }
     }
 };
 
 
 education.display = function() {
-    for(var school = 0; school < education.schools.length; school++) {
+    for (var school = 0; school < education.schools.length; school++) {
         $("#education").append(HTMLschoolStart);
         var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
         var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
@@ -160,7 +149,7 @@ function inName(name) {
     var newName = name;
     newName = newName[0].toUpperCase() + newName.slice(1, newName.indexOf(" ") + 1).toLowerCase() + newName.slice(newName.indexOf(" ") + 1).toUpperCase();
     return newName;
-};
+}
 
 //call functions to display relevant data
 bio.display();
